b5f130d6d57856562341b3d9a9da1ece142afa9c
Austen Manser <42944188+Amanser@users.noreply.github.com>
2018-12-30 15:48:15 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,45 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
+
 # Create an instance of Flask
 app = Flask(__name__)
 
+
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_info")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
+mongo = PyMongo(app)
+
+
+
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    mars_info = mongo.db.mars_info.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars_info=mars_info)
+
+
+
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_info = mongo.db.mars_info
+
+    mars_data = scrape_mars.scrape_news()
+    mars_data = scrape_mars.scrape_feature()
+    mars_data = scrape_mars.scrape_facts()
+    mars_data = scrape_mars.scrape_weather()
+    # mars_data = scrape_mars.scrape_hemispheres()
+
+    mars_info.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup 
 from splinter import Browser
 import pandas as pd 
-import requests
 import time 
 
 
@@ -22,24 +21,29 @@ def scrape_news():
     
     browser.visit(url)
 
-    time.sleep(1)
+    # Add a delay to give the chromedriver time to connect
+    time.sleep(3)
 
     html = browser.html
     
     soup = BeautifulSoup(html, "html.parser")
 
+    # Scrape news title
     news_title = soup.find('div', class_='content_title').find('a').text.strip()
 
+    # Scrape news paragraph
     news_p = soup.find_all(class_="rollover_description_inner")[0].text.strip()
 
+    # Assign scraped data to the dictionary
     mars_info['news_title'] = news_title
-
     mars_info['news_p'] = news_p
 
     # Close the browser after scraping
     browser.quit()
 
     return mars_info
+
+     
 
 def scrape_feature():
 
@@ -49,6 +53,7 @@ def scrape_feature():
     
     browser.visit(url)
 
+    # Add a delay to give the chromedriver time to connect
     time.sleep(1)
 
     html = browser.html
@@ -59,8 +64,10 @@ def scrape_feature():
 
     url_2 = soup.find_all('img', class_='thumb')[0]['src']
 
+    # Append the final image url
     featured_image_url = url_1 + url_2
 
+    # Add featured image url to dictionary
     mars_info['featured_image'] = featured_image_url
 
     # Close the browser after scraping
@@ -83,8 +90,10 @@ def scrape_weather():
     
     soup = BeautifulSoup(html, 'html.parser')
 
+    # Scrape weather data from latest Tweet
     mars_weather = soup.find_all('p', class_='TweetTextSize TweetTextSize--normal js-tweet-text tweet-text')[0].text
 
+    # Add weather info from Twitter to dictionary
     mars_info['weather'] = mars_weather
 
     # Close the browser after scraping
@@ -109,71 +118,75 @@ def scrape_facts():
     # Set the index to the `Description` column
     mars_df.set_index('Description', inplace=True)
 
+    convertDFToHTML = mars_df.to_html()
     # Add the df to the mars_info dictionary
-    mars_info['mars_facts'] = mars_df
+    mars_info['mars_facts'] = convertDFToHTML
 
     return mars_info
 
+# NOTE: The astrogeology.usgs.gov website was down when trying to complete this section. A cached version from web.archive.org had to be used.
+        # I was able to get the cached version to work in the Jupyter Notebook, but not inside of the scrape function.
 
-def scrape_hemisphere():
+# def scrape_hemispheres():
 
-    browser = init_browser()
+#     browser = init_browser()
     
-    hemispheres_url = 'http://web.archive.org/web/20181114171728/https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+#     hemispheres_url = 'http://web.archive.org/web/20181114171728/https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
 
-    browser.visit(hemispheres_url)
+#     browser.visit(hemispheres_url)
 
-    time.sleep(1)
+#     time.sleep(1)
 
-    html_hemispheres = browser.html
+#     html_hemispheres = browser.html
 
-    # Parse HTML with Beautiful Soup
-    soup = BeautifulSoup(html_hemispheres, 'html.parser')
+#     # Parse HTML with Beautiful Soup
+#     soup = BeautifulSoup(html_hemispheres, 'html.parser')
 
-    # Retreive all items that contain mars hemispheres information
-    items = soup.find_all('div', class_='item')
+#     # Retreive all items that contain mars hemispheres information
+#     items = soup.find('a', class_='itemLink product-item')['href']
 
-    new_url = 'http://web.archive.org' + items
+#     new_url = 'http://web.archive.org' + str(items)
 
-    browser.visit(new_url)
+#     browser.visit(new_url)
 
-    time.sleep(1)
+#     time.sleep(1)
 
-    html_hemispheres = browser.html
+#     html_hemispheres = browser.html
 
-    soup = BeautifulSoup(html_hemispheres, 'html.parser')
+#     soup = BeautifulSoup(html_hemispheres, 'html.parser')
 
-    # Retrieve the url for the full jpeg image
-    hemi_img = soup.find('div', class_='wide-image-wrapper').find('li').find('a')['href']
+#     # Retrieve the url for the full jpeg image
+#     hemi_img = soup.find('div', class_='wide-image-wrapper').find('li').find('a')['href']
 
-    hemisphere_image_urls = []
+#     hemisphere_image_urls = []
 
-    for x in items: 
-        # Store title
-        title = x.find('h3').text
+    # Loop to grab all hemisphere images and titles
+#     for x in items: 
+#         # Store title
+#         title = x.find('h3').text
         
-        # Store link that leads to full image website
-        partial_img_url = x.find('a', class_='itemLink product-item')['href']
+#         # Store link that leads to full image website
+#         partial_img_url = x.find('a', class_='itemLink product-item')['href']
         
-        # Visit the link that contains the full image website 
-        new_url = 'http://web.archive.org' + partial_img_url
+#         # Visit the link that contains the full image website 
+#         new_url = 'http://web.archive.org' + partial_img_url
         
-        browser.visit(new_url)
+#         browser.visit(new_url)
         
-        # HTML Object of individual hemisphere information website 
-        partial_img_html = browser.html
+#         # HTML Object of individual hemisphere information website 
+#         partial_img_html = browser.html
         
-        # Parse HTML with Beautiful Soup for every individual hemisphere information website 
-        soup = BeautifulSoup( partial_img_html, 'html.parser')
+#         # Parse HTML with Beautiful Soup for every individual hemisphere information website 
+#         soup = BeautifulSoup( partial_img_html, 'html.parser')
         
-        # Retrieve full image source 
-        hemi_img = soup.find('div', class_='wide-image-wrapper').find('li').find('a')['href']
+#         # Retrieve full image source 
+#         hemi_img = soup.find('div', class_='wide-image-wrapper').find('li').find('a')['href']
         
-        # Append the retreived information into a list of dictionaries 
-        hemisphere_image_urls.append({"title" : title, "img_url" : hemi_img})
+#         # Append the retreived information into a list of dictionaries 
+#         hemisphere_image_urls.append({"title" : title, "img_url" : hemi_img})
     
 
-    # Display hemisphere_image_urls
-    mars_info['hemispheres'] = hemisphere_image_urls
+#     # Display hemisphere_image_urls
+#     mars_info['hemispheres'] = hemisphere_image_urls
 
-    return mars_info
+#     return mars_info
